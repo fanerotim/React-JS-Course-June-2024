ceab8ddfa2dec3d104ddf29fdc299a0c3275d04c
Petar Cholakov <petar.cholakov30@gmail.com>
2024-07-07 21:15:47 +0100

    set up basic example

--- useEffect, useState/src/App.jsx
+++ useEffect, useState/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import Home from './components/home/Home'
 
 function App() {
 
-
   return (
     <>
-      
+      <h1>Enter your name below</h1>
+      <br/>
+      <Home/>
     </>
   )
 }
--- useEffect, useState/src/components/home/Home.jsx
+++ useEffect, useState/src/components/home/Home.jsx
@@ -1,3 +1,28 @@
+import { useState, useEffect } from 'react'
+import './Home.css'
+
 const Home = () => {
-    
+
+    const [inputValue, setInputValue] = useState('');
+    const [name, setName] = useState(false);
+
+    useEffect(() => {
+        console.log('Component re-rendered')
+    }, [inputValue])
+
+    function handleShowNameClick() {
+        setInputValue(inputValue)
+        setName(!name)
+    }
+
+    return (
+        <section>
+            <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+            <br />
+            <button onClick={handleShowNameClick} className='btn'>{name ? 'Hide name' : 'Show name'}</button>
+            {name && <h3>Your name is {inputValue}</h3>}
+        </section>
+    )
 }
+
+export default Home;
