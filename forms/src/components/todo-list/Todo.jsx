72b5f06f33a08996421b7c2750ef041c18e48695
Petar Cholakov <petar.cholakov30@gmail.com>
2024-07-13 17:12:22 +0100

    set up complete functionality

--- forms/src/components/todo-list/Todo.jsx
+++ forms/src/components/todo-list/Todo.jsx
@@ -8,16 +8,19 @@ export const Todo = ({ todo }) => {
             text: 'Go to training',
             isCompleted: false,
             index: 0,
+            'text-decoration': false
         },
         {
             text: 'Avoid eating a lot of chocolate',
             isCompleted: false,
             index: 1,
+            'text-decoration': false
         },
         {
             text: 'Practice React forms',
             isCompleted: false,
             index: 2,
+            'text-decoration': false
         }
     ])
 
@@ -25,7 +28,8 @@ export const Todo = ({ todo }) => {
         todos.push({
             text: todo,
             isCompleted: false,
-            index: todos.length
+            index: todos.length,
+            'text-decoration': false
         })
     }
 
@@ -35,15 +39,24 @@ export const Todo = ({ todo }) => {
         setTodos(newTodos);
     }
 
+    function handleComplete(index) {
+        const newTodos = [...todos];
+        newTodos[index]['text-decoration'] = true;
+        setTodos(newTodos)
+        console.log(todos);
+    }
+
     return (
         <ul className='todo-wrapper'>
 
             {todos.map((todo) => (
                 <div key={todo.index} className='todo-item-wrapper'>
-                    <li className='todo-item' key={todo.index}>{todo.text}</li>
+                    <li style={{textDecoration: todo['text-decoration'] ? 'line-through' : ''}} className='todo-item' key={todo.index}>{todo.text}</li>
 
                     <div className='buttons-wrapper'>
-                        <button className='complete-btn btn'>Complete</button>
+                        <button onClick={() => {
+                            handleComplete(todo.index)
+                        }} className='complete-btn btn'>Complete</button>
                         <button onClick={() => {
                             handleDelete(todo.index)
                         }} index={todo.index} className='delete-btn btn'>Delete</button>
